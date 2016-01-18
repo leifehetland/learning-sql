dc83c87d31c7e0af057703cf8b2538da3d92bc2e
Leif E. Hetland <lehetland1@gmail.com>
2016-01-18 16:04:40 -0600

    Shows invoices of customers who are from Brazil

---
+++ 3.sql
@@ -0,0 +1,1 @@
+SELECT Customer.FirstName, Customer.LastName, Invoice.InvoiceId, Invoice.InvoiceDate, Invoice.BillingCountry FROM Customer JOIN Invoice ON Customer.CustomerId == Invoice.CustomerId WHERE Country == 'Brazil'
